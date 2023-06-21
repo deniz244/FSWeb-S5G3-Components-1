b7d882005fbb9908c3810dd665e1d15c2c97bb46
Deniz <77787415+deniz244@users.noreply.github.com>
2023-06-21 11:49:21 +0300

    menu js görevi denendi

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -11,9 +11,9 @@ let menuElemanlari = [
 ];
 
 /*
-  Adım 1: Aşağıdaki etiketler gibi bir menü oluşturmak için 'menuYapici' adlı bir bileşen yazın:
+Adım 1: Aşağıdaki etiketler gibi bir menü oluşturmak için 'menuYapici' adlı bir bileşen yazın:
 
-  <div class="menu">
+<div class="menu">
     <ul>
       {tüm menü elemanları <li> içlerine}
     </ul>
@@ -21,16 +21,40 @@ let menuElemanlari = [
 
   'menuYapici' fonksiyonu tek argümanı olarak bir menü elemanları dizisini alır (birinci parametre).
 
-  Adım 2: Fonksiyonun içinde, dizideki her öğe için bir liste <li> öğesi oluşturarak dizi üzerinde yineleme yapın.
-  Tüm bu öğeleri <ul>'a ekleyin
+Adım 2: Fonksiyonun içinde, dizideki her öğe için bir liste <li> öğesi oluşturarak dizi üzerinde yineleme yapın.
+Tüm bu öğeleri <ul>'a ekleyin
 
-  Adım 3: Hala fonksiyon içindeyiz, DOM'dan menü düğmesini seçin (`index.html` içinde `menu-button` sınıfına (class) sahip öğe).
+Adım 3: Hala fonksiyon içindeyiz, DOM'dan menü düğmesini seçin (`index.html` içinde `menu-button` sınıfına (class) sahip öğe).
 
-  Adım 4: Menü butonuna bir `click` event dinleyicisi ekleyin. Butona her tıklanıldığında div.menu classına `menu--open`('menu' class'ına sahip olan div) ekleyip/silecek(toogle).
-  
-  İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
+Adım 4: Menü butonuna bir `click` event dinleyicisi ekleyin. Butona her tıklanıldığında div.menu classına `menu--open`('menu' class'ına sahip olan div) ekleyip/silecek(toogle).
 
-  Adım 5: Oluştrulan div.menu 'yü döndürmeyi unutmayın.
+İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
 
-  Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
+Adım 5: Oluştrulan div.menu 'yü döndürmeyi unutmayın.
+
+Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
 */
+
+function menuYapici(arr) {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  const ul = document.createElement("ul");
+
+  const list = document.createElement("li");
+
+  arr.map(list);
+
+  const button = document.getElementsByClassName("menu-button");
+  button.addEventListener("click", (e) => {
+    console.log("clicked!");
+  });
+
+  menu.appendChild(ul);
+  menu.appendChild(list);
+  return menu;
+}
+
+document
+  .getElementsByClassName("header")
+  .appendChild(menuYapici(menuElemanlari));
